8c59d3822c767e12d265d8e6892d7fe8f7f2b361
zhang-xiaoyan <zhangxiaoyan0606@foxmail.com>
2016-02-01 16:28:41 +0800

    change drag.js

--- drag.js
+++ drag.js
@@ -5,5 +5,8 @@ function drag(id){
     var obj=document.getElementById(id);
     var disX=0;
     var disY=0;
-    obj.onmousedown=function(){}
+    obj.onmousedown=function(ev){
+        disX=ev.clientX-obj.offsetLeft;
+        disY=ev.clientY-obj.offsetTop;
+    }
 }
